957eb1846e09bde30167251c5dd9cbf66ae02c6e
Elisha Vasserman <Elisha Vasserman>
2018-11-24 20:17:53 +0200

    Tracking changes of property binding

--- src/app/child-one.component.ts
+++ src/app/child-one.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChange } from '@angular/core';
 
 @Component({
     selector: 'child-one',
@@ -10,18 +10,34 @@ import { Component, Input, OnInit } from '@angular/core';
     styles: ['#name {color: red; font-weight: bold;}']
   })
   
-  export class ChildOne implements OnInit{
+  export class ChildOne implements OnInit, OnChanges {
     
     @Input() parentToChildOne : string;
     @Input() num1 : number;
     @Input() num2 : number;
 
     title : string = 'Page' ;
+    changeLog: string[] = [];
     
 
     ngOnInit() {
         this.title = this.parentToChildOne;
         console.log(this.title);
+    }
+
+    ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+      let log: string[] = [];
+      for (let propName in changes) {
+        let changedProp = changes[propName];
+        let to = JSON.stringify(changedProp.currentValue);
+        if (changedProp.isFirstChange()) {
+          log.push(`Initial value of ${propName} set to ${to}`);
+        } else {
+          let from = JSON.stringify(changedProp.previousValue);
+          log.push(`${propName} changed from ${from} to ${to}`);
+        }
+      }
+      this.changeLog.push(log.join(', '));
     }
   }
   
